fc4803e4289109c7a3394a135dacd022436b131b
Graham Cottridge <codinggc@gmail.com>
2018-07-24 21:53:41 +0100

    Fixed syntax errors

--- src/reducers.js
+++ src/reducers.js
@@ -1,4 +1,4 @@
-import { CHANGE_SEARCH_FIELD } = from './constants,js';
+import { CHANGE_SEARCH_FIELD } from './constants.js';
 
 const initialState = {
   SearchField: ''
@@ -6,8 +6,8 @@ const initialState = {
 
 export const searchRobots = (state=initialState, action={}) => {
   switch(action.type) {
-    case: CHANGE_SEARCH_FIELD:
-      return Object.assign({}, state, SearchField: action.payload)
+    case CHANGE_SEARCH_FIELD:
+      return Object.assign({}, state, { SearchField: action.payload });
     default:
       return state;
   }
