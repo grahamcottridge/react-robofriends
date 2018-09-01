6237d2231bd5281ba38dbae4c34191ba9099840a
Graham Cottridge <codinggc@gmail.com>
2018-09-01 22:28:26 +0100

    Implement redux connect()

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import './index.css';
+
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import { searchRobots } from './reducers';
--- src/reducers.js
+++ src/reducers.js
@@ -1,13 +1,13 @@
 import { CHANGE_SEARCH_FIELD } from './constants.js';
 
 const initialState = {
-  SearchField: ''
+  searchField: ''
 }
 
 export const searchRobots = (state=initialState, action={}) => {
   switch(action.type) {
     case CHANGE_SEARCH_FIELD:
-      return Object.assign({}, state, { SearchField: action.payload });
+      return Object.assign({}, state, { searchField: action.payload });
     default:
       return state;
   }
